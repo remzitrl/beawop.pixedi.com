a6c6b1642f21e96f6c17314be9a7d19fe4892d99
remzitrl <aremzitural@gmail.com>
2025-10-21 02:45:05 +0300

    update

--- src/components/blog.js
+++ src/components/blog.js
@@ -14,7 +14,7 @@ const Blog = () => {
                 <div className="row align-items-stretch">
                     <div className="col-12 col-md-6 gallery__body-two d-flex" data-aos="fade-right" data-aos-duration="1000">
                         <div className="mb-4 mb-md-0 w-100">
-                            <div className="gallery__body-layer d-block h-100">
+                            <div className="gallery__body-layer-no-click d-block h-100">
                                 <img className="img-fluid w-100 gallery__body-two--img" src="/assets/img/gal1.webp" alt="image" style={{height: '100%', objectFit: 'cover'}} />
                             </div>
                         </div>
@@ -22,17 +22,17 @@ const Blog = () => {
                     <div className="col-12 col-md-6 gallery__body-two d-flex" data-aos="fade-left" data-aos-duration="1000">
                         <div className="row w-100">
                             <div className="col-12 col-sm-6 mb-4 mb-md-0">
-                                <div className="gallery__body-layer d-block h-100">
+                                <div className="gallery__body-layer-no-click d-block h-100">
                                     <img className="img-fluid w-100 gallery__body-two--img" src="/assets/img/gal2.webp" alt="image" style={{height: '100%', objectFit: 'cover'}} />
                                 </div>
                             </div>
                             <div className="col-12 col-sm-6 mb-4 mb-md-1">
-                                <div className="gallery__body-layer d-block h-100">
+                                <div className="gallery__body-layer-no-click d-block h-100">
                                     <img className="img-fluid w-100 gallery__body-two--img" src="/assets/img/gal3.webp" alt="image" style={{height: '100%', objectFit: 'cover'}} />
                                 </div>
                             </div>
                             <div className="col-12 col-sm-12 mt-md-3 mt-xxl-4">
-                                <div className="gallery__body-layer d-block h-100">
+                                <div className="gallery__body-layer-no-click d-block h-100">
                                     <img className="img-fluid w-100 gallery__body-two--img" src="/assets/img/gal4.webp" alt="image" style={{height: '100%', objectFit: 'cover'}} />
                                 </div>
                             </div>
